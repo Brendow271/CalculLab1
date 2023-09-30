aa4a91613b4881f6fdcafae4ae510d25c0288ccb
Brendow271 <pashashkuropat@yandex.ru>
2023-10-01 01:51:02 +0300

    Main funcs:Calculator and Operation with dll Loader

---
+++ cmake-build-debug/Operation.cpp
@@ -0,0 +1,11 @@
+#include "Operation.h"
+
+Operation::Operation() {
+    operations["+"] = [](double a, double b)->double {return b + a; };
+    operations["-"] = [](double a, double b)->double {return b - a; };
+    operations["*"] = [](double a, double b)->double {return b * a; };
+    operations["/"] = [](double a, double b)->double {return b / a; };
+};
+double Operation::operation(double const& a, std::string const& name, double const& b) {
+    return operations[name](a, b);
+};
---
+++ cmake-build-debug/Operation.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#include<iostream>
+#include<map>
+#include<functional>
+#include <iostream>
+#include <stack>
+#include <cctype>
+#include <map>
+#include <algorithm>
+#include <string>
+using mapOperators = std::map<std::string, std::function<double(double, double)>>;
+class Operation {
+    Operation(const Operation&) = delete;
+    Operation& operator=(Operation const&) = delete;
+    mapOperators operations;
+public:
+    Operation();
+    double operation(double const& a, std::string const& name, double const& b);
+};
